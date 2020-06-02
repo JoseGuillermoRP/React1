c287f0c15e5cb91e0584eb9ba480f6481d725afb
Jose Rojas Paredes <jrojasp1992@gmail.com>
2020-06-01 22:22:53 -0500

    mi primer app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,39 @@
 import React from 'react';
 import './App.css';
 
-/* function Helloworld(props) { 
-return(
-<div id="hola">
-<h3>{props.subtitle}</h3>
-{props.mytext}
-</div> 
-) 
- } */
-class Helloworld extends React.Component{
+import tasks from './sample/tasks.json';
+
+//COMPONENTES
+
+import Tasks from './components/Tasks';
+import TaskForm from './components/TaskForm';
+
+class App extends React.Component{
 
 state = {
-  show: false
+  tasks: tasks
+}
+ addTask = (title,description) =>{
+   const newTask = {
+      
+      title:title,
+      description:description,
+     id: this.state.tasks.length
+   }
+  this.setState({
+    tasks:[...this.state.tasks, newTask]
+  })
+ }
+
+render(){
+  
+  return <div>
+    <TaskForm addTask={this.addTask}/>
+  <Tasks tasks={this.state.tasks} />
+  </div>
+}
 }
 
-  render(){
-    if (this.state.show){
-      return(
-        <div id="hola">
-        <h3>{this.props.subtitle}</h3>
-        {this.props.mytext}
-        </div>
-      )
-    }else{
-      return <h1>There are not elements</h1>
-    }
-    
-  }
-}
-
-function App() {
-  return (
-    <div>
-      This is may component:
-      <Helloworld mytext="Hello Fazt" subtitle="loremp ispum"/>
-       <Helloworld mytext="Hola mundo" subtitle="asadsad"/>
-        <Helloworld mytext="Hello!" subtitle="component three"/>
-        </div>
-  );
-}
 
 
 export default App;
